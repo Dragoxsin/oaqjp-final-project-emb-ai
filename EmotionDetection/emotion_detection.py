135382cb5ec6f225d5a379bb8a142f8bf4175386
Dragoxsin <mujtaba.10059@gmail.com>
2026-09-11 13:32:20 -0400

    Save latest Flask emotion detection updates

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,10 +2,40 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    myobj = {'raw_document': {'text': text_to_analyze} }
-    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    if not text_to_analyze or text_to_analyze.strip() == "":
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
+
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+
+    myobj = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+
     response = requests.post(url, json=myobj, headers=headers)
+
+    if response.status_code == 400:
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
 
     formatted_response = json.loads(response.text)
 
@@ -19,7 +49,11 @@ def emotion_detector(text_to_analyze):
 
     dominant_emotion = max(emotions, key=emotions.get)
 
-    return {"anger": anger_score, "disgust": disgust_score, "fear": fear_score, "joy": joy_score, "sadness": sadness_score, "dominant_emotion": dominant_emotion,}
-
-
-    
+    return {
+        "anger": anger_score,
+        "disgust": disgust_score,
+        "fear": fear_score,
+        "joy": joy_score,
+        "sadness": sadness_score,
+        "dominant_emotion": dominant_emotion
+    }
